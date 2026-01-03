59b64c59b9355bcab3cc940153ccbc84e68757a5
chinkauchenna2021 <uchennachinka4@gmail.com>
2026-01-03 17:03:36 +0100

    REMOVE PRELOADER

--- src/components/mainpage/Layout.tsx
+++ src/components/mainpage/Layout.tsx
@@ -131,7 +131,7 @@ export default function Layout({ children }: LayoutProps) {
       </div>
       <div>
         <div>
-          <Preloader />
+          {/* <Preloader /> */}
           <div className='page-wrapper'>
             <Header />
             {children}
