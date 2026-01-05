d44c157331f5652c4bcb13d9b95314e81e9bf800
chinkauchenna2021 <uchennachinka4@gmail.com>
2026-01-05 18:20:49 +0100

    FIX TOKEN SAVING

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect } from 'react';
-import { useAuthStore } from '../stores/auth.store';
+import { useAuthStore } from '@/stores/auth.store';
 
 export const useAuth = () => {
   const {
--- src/hooks/useTokenStorage.ts
+++ src/hooks/useTokenStorage.ts
@@ -1,3 +1,4 @@
+'use client';
 import useLocalStorage from 'use-local-storage';
 
 export interface TokenData {
